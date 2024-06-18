248ca1de8e95743b0044c37fd601eebb596e8ef8
Anthony <anthony.oliveira5@hotmail.com>
2024-06-18 20:27:14 -0300

    Commit inicial, iniciando o projeto

---
+++ src/main/java/com/barbearia/barbeariaAPI/BarbeariaApiApplication.java
@@ -0,0 +1,13 @@
+package com.barbearia.barbeariaAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbeariaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarbeariaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/barbearia/barbeariaAPI/BarbeariaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barbearia.barbeariaAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbeariaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
